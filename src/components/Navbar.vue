04b8265e49cbf63439bb92d29aa7e3d1ab16bb5b
JordanHubb <Fenstersteuerungber SupportAhmad.Matar@Student.HTW-Berlin.de>
2023-06-28 14:05:47 +0200

    test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,9 @@
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Your Party</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
          <router-link class="nav-link" to="/">Home</router-link>
